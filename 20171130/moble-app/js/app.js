53ec34d1781279a0a190b2efa2c4f181ac103055
mdlawrenceusa <mdlawrence@me.com>
2017-12-07 17:01:16 -0500

    done

--- 20171130/moble-app/js/app.js
+++ 20171130/moble-app/js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+
+var app = angular.module('GalleryApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'HomeController',
+      templateUrl: 'views/home.html'
+    })
+    .when('/photos/:id', {
+      controller: 'PhotoController',
+      templateUrl: 'views/photo.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
